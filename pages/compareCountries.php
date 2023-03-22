b1276b774e2127fede3de9058718ee534f30a7a9
Eduardo Lima <eduardolima.tecnico@gmail.com>
2023-03-22 11:45:07 -0300

    refactor: :art: Estilização do arquivo de comparação

--- pages/compareCountries.php
+++ pages/compareCountries.php
@@ -4,6 +4,8 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../view/css/style.css">
+  <link rel="icon" href="src/img/icon.ico">
   <title>Kidopi - Painel Covid-19</title>
 </head>
 <body>
