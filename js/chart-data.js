a29ccbf4593cc74423ca62e1d09ae6374c9a65c4
Eduardo Lima <eduardolima.tecnico@gmail.com>
2023-03-24 00:01:26 -0300

    feat: :zap: Criação de variáveis e padronização

--- js/chart-data.js
+++ js/chart-data.js
@@ -1,7 +1,7 @@
-var randomScalingFactor = function(){ return Math.round(Math.random()*1000)};
+var randomScalingFactor = function () { return Math.round(Math.random() * 1000) };
 	
 	var lineChartData = {
-		labels : ["January","February","March","April","May","June","July","Teste"],
+		labels : states,
 		datasets : [
 			{
 				label: "My First dataset",
@@ -11,7 +11,7 @@ var randomScalingFactor = function(){ return Math.round(Math.random()*1000)};
 				pointStrokeColor : "#fff",
 				pointHighlightFill : "#fff",
 				pointHighlightStroke : "rgba(220,220,220,1)",
-				data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
+				data : confirmed
 			},
 			{
 				label: "My Second dataset",
@@ -21,28 +21,28 @@ var randomScalingFactor = function(){ return Math.round(Math.random()*1000)};
 				pointStrokeColor : "#fff",
 				pointHighlightFill : "#fff",
 				pointHighlightStroke : "rgba(37, 190, 174, 1)",
-				data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
+				data : death
 			}
 		]
 
 	}
 		
 	var barChartData = {
-		labels : ["January","February","March","April","May","June","July","Teste"],
+		labels : states,
 		datasets : [
 			{
-				fillColor : "rgba(220,220,220,0.5)",
-				strokeColor : "rgba(220,220,220,0.8)",
-				highlightFill: "rgba(220,220,220,0.75)",
-				highlightStroke: "rgba(220,220,220,1)",
-				data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
+				fillColor : "rgba(143, 140, 136,0.5)",
+				strokeColor : "rgba(143, 140, 136,0.8)",
+				highlightFill: "rgba(143, 140, 136,0.75)",
+				highlightStroke: "rgba(143, 140, 136,1)",
+				data : confirmed
 			},
 			{
-				fillColor : "rgba(37, 190, 174, 0.2)",
-				strokeColor : "rgba(37, 190, 174, 0.8)",
-				highlightFill : "rgba(37, 190, 174, 0.75)",
-				highlightStroke : "rgba(37, 190, 174, 1)",
-				data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
+				fillColor : "rgba(226, 105, 71, 0.2)",
+				strokeColor : "rgba(226, 105, 71, 0.8)",
+				highlightFill : "rgba(226, 105, 71, 0.75)",
+				highlightStroke : "rgba(226, 105, 71, 1)",
+				data : death
 			}
 		]
 
